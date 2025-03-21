2edb2526109121dfd678a7384f109822c22897fd
priyathamkatta06 <priyathamkatta@randomwalk.ai>
2025-03-21 18:22:35 +0530

    add new tables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
 	dbCredentials: {
 		url: process.env.DATABASE_URL
 	},
+	// tablesFilter: ['*', '!analytics', '!cameras', '!detections', '!gate_monitoring'],
 
 	verbose: true,
 	strict: true,

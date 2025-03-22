3336d97be5f5356b4eaab6ac45713d9695829cba
D Abishek <abishek.d@randomwalk.ai>
2025-03-22 18:48:48 +0530

    Env used differently

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -2,11 +2,12 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from './db';
 import * as schema from './db/schema';
-import { PUBLIC_APP_NAME, PUBLIC_BETTER_AUTH_URL } from '$env/static/public';
+// import { PUBLIC_APP_NAME, PUBLIC_BETTER_AUTH_URL } from '$env/static/public';
+import { env } from '$env/dynamic/private';
 
 export const auth = betterAuth({
-	appName: PUBLIC_APP_NAME,
-	baseURL: PUBLIC_BETTER_AUTH_URL,
+	appName: env.PUBLIC_APP_NAME,
+	baseURL: env.PUBLIC_BETTER_AUTH_URL,
 	database: drizzleAdapter(db, {
 		provider: 'pg', // or "mysql", "sqlite",
 		schema
